506290649faa09f3bba34f726ca3fdf3537e52af
mindest <linminuser@gmail.com>
2021-11-11 09:23:28 +0000

    refactor wrapper hook, handle other types; pickle load

--- tools/python/micro_benchmark/patching_hook.py
+++ tools/python/micro_benchmark/patching_hook.py
@@ -4,6 +4,9 @@ import inspect as ins
 import pickle
 
 
+WRAPPING_PREFIX = 'Wrap_'
+
+
 def isfunc(mod, f):
     assert hasattr(mod, f)
     attr = getattr(mod, f)
@@ -12,13 +15,18 @@ def isfunc(mod, f):
         if f[0] == "_": return False # Temporarily exclude functions starting with '_'
         # if f[:2] == '__' and f[-2:] == '__': return False
 
-    # Add functions to this list if they cause recursion
+    # Ignore functions to this list if they cause recursion
     ignore = ['size', 'tolist', 'dim', 'is_storage', 'item']
+    # Also ignore the following functions related to tensor initialization
+    ignore += ['zero_', 'uniform_', 'normal_', 'fill_']
+    # and more
+    ignore += ['copy_', 'numel', 'set_', 'has_names', 'index_select', 'cuda', 'contiguous', 'detach',
+               'view_as', 'is_floating_point', 'float', 'half', 'to']
     if f in ignore:
         return False
 
-    # Temporarily exclude other functions/methods from `torch.Tensor`
-    if mod == torch.Tensor and f not in ['repeat', 'view', 'cumsum', 'ne']:
+    ignore_patterns = ['storage', 'stride', 'has_torch_function', 'new', 'as']
+    if any([s in f for s in ignore_patterns]):
         return False
 
     return ins.ismethod(attr) or ins.isfunction(attr) or ins.ismethoddescriptor(attr) or ins.isbuiltin(attr)
@@ -29,8 +37,9 @@ def add_wrapper(mod, name):
         func = getattr(mod, name)
         def forward(self, *args, **kwargs):
             return func(*args, **kwargs)
+        wrapping_class_name = WRAPPING_PREFIX + func.__name__
         def wrapper_func(*args, **kwargs):
-            Wrap = type(func.__name__, (nn.Module,), dict(forward=forward))
+            Wrap = type(wrapping_class_name, (nn.Module,), dict(forward=forward))
             return Wrap()(*args, **kwargs)
         setattr(mod, name, wrapper_func)
     except:
@@ -46,14 +55,17 @@ def init():
     for mod in [torch.Tensor, torch.nn.functional]:
         patching(mod)
 
-def hook(module, inputs, export_file='/tmp/picklefile'):
-    module_str = repr(module)
-    if not module_str.startswith('_') and module_str.lower() == module_str and module_str != 'wrapper_func()':
-        print('module:', module)
+
+def hook(module, inputs, export_file='/tmp/picklefile', print_info=True):
+    module_str = repr(module).rstrip('()')
+    if module_str.startswith(WRAPPING_PREFIX):
+        module_name = module_str[len(WRAPPING_PREFIX):]
+        if print_info: print('module:', module, sep=' ')
         values = []
+        save_dict = True
         for inp in inputs:
             if torch.is_tensor(inp):
-                if inp.shape == torch.Size([]):
+                if inp.shape == torch.Size([]): # Treat tensor with no size as a number
                     key = 'numeric'
                     value = inp.item()
                 else:
@@ -61,15 +73,44 @@ def hook(module, inputs, export_file='/tmp/picklefile'):
                     value = inp.shape
             else:
                 value = inp
-                if isinstance(inp, tuple):
-                    key = 'tuple'
-                elif isinstance(inp, list):
-                    key = 'list'
-                else:
+                inp_type = type(inp)
+                if inp_type in (tuple, list, str, bool):
+                    key = inp_type.__name__
+                elif inp_type in (int, float, complex):
                     key = 'numeric'
+                elif inp is None:
+                    key = 'none'
+                else:
+                    # Skip unknown data type case
+                    print(f'In module {module_name}, input type {inp_type.__name__} not supported.')
+                    save_dict = False
+                    break
             values.append([key, value])
-            print(key, value)
-        module_name = module_str.rstrip('()')
-        dict_to_save = {'op': module_name, 'inputs': values}
-        with open(export_file, 'ab') as f:
-            pickle.dump(dict_to_save, f)
+            # if print_info: print(key, value)
+        if save_dict:
+            dict_to_save = {'op': module_name, 'inputs': values}
+            with open(export_file, 'ab') as f:
+                pickle.dump(dict_to_save, f)
+
+def load_pickle(pickle_file='/tmp/picklefile'):
+    '''
+    Loads input info.
+
+    Returns: a list of input info dicts.
+    '''
+    input_info = {}
+    with open(pickle_file, 'rb') as f:
+        while True:
+            try:
+                x = pickle.load(f)
+                op = x['op']
+                info = x['inputs']
+                if op in input_info:
+                    input_info[op].append(info)
+                else:
+                    input_info[op] = [info]
+            except EOFError:
+                break
+
+    print('Operator list:', list(input_info.keys()))
+    return input_info
